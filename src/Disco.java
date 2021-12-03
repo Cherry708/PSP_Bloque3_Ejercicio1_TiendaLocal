407419354cbe47c984ee4d7e9184ab991782a456
Cherry708 <aluser8241@ieselcaminas.org>
2021-12-03 19:12:05 +0100

    Progreso

--- src/Disco.java
+++ src/Disco.java
@@ -1,9 +1,15 @@
+import java.io.RandomAccessFile;
+
 public class Disco {
     private int codigo;
     private String titulo;
     private String autor;
     private double  precio;
     private int cantidadEjemplares;
+
+    public Disco() {
+
+    }
 
     public Disco(int codigo, String titulo, String autor, double precio, int cantidadEjemplares){
         this.codigo = codigo;
@@ -13,26 +19,44 @@ public class Disco {
         this.cantidadEjemplares = cantidadEjemplares;
     }
 
+
+
     public void leeDeTeclado(){
         //TODO implementar logica
     }
 
     @Override
     public String toString() {
-        return "PruebaTiendaLocal{" +
-                "codigo=" + codigo +
-                ", titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                ", precio=" + precio +
-                ", cantidadEjemplares=" + cantidadEjemplares +
-                '}';
+        return "TITULO: " + this.titulo + ". CODIGO: " + this.codigo + "\n"
+                + "AUTOR: " + this.autor + "\n"
+                + "PRECIO: " + this.precio + "CANTIDAD: " + this.cantidadEjemplares;
     }
 
-    public void escribeEnFichero(){
+    public void escribeEnFichero(RandomAccessFile stream){
         //TODO implemetar logica
     }
 
-    public void leeDeFichero(){
+    public void leeDeFichero(RandomAccessFile stream){
         //TODO implementar logica
+    }
+
+    public int getCantidadEjemplares() {
+        return cantidadEjemplares;
+    }
+
+    public void setCantidadEjemplares(int cantidadEjemplares) {
+        this.cantidadEjemplares = cantidadEjemplares;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public int getCantidad() {
+        return this.cantidadEjemplares;
     }
 }
